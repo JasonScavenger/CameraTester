5f27af990507d79d9dc0c7cf38d017b90030d8e2
TrushinVA <trushinva@mositlab.ru>
2025-03-25 18:45:25 +0300

    улучшена загрузка, скелет для функции

--- mainTester/mainTester.py
+++ mainTester/mainTester.py
@@ -6,26 +6,53 @@ from PIL import ImageTk, Image
 from tkinter import filedialog
 import os
 
+root = Tk()     # создаем корневой объект - окно
+# Влад Аким Никита Камилла Слава
+root.title("VANKS Camera Tester")     # устанавливаем заголовок окна
+root.geometry("400x350")    # устанавливаем размеры окна
+ 
+label = Label(text="здарова бандиты") # создаем текстовую метку
+label.pack(anchor=NW)    # размещаем метку в окне
+
+panel = Label(root)
+
+ImageToAnalyze = None
+
 def openfn():
     filename = filedialog.askopenfilename(title='open')
     return filename
 
 def open_img():
+    global panel
+    global ImageToAnalyze
+
     filename = openfn()
-    img = Image.open(filename)
-    #img = img.resize((250, 250), Image.)
+    ImageToAnalyze = Image.open(filename)
+    scale = ImageToAnalyze.height / ImageToAnalyze.width
+    if (scale > 1):
+        height = 256
+        width = (int)(height / scale)
+    else:
+        width = 256
+        height = (int)(width * scale)
+    img = ImageToAnalyze.resize((width, height), Image.Resampling.BILINEAR)
     img = ImageTk.PhotoImage(img)
+    panel.destroy()
     panel = Label(root, image=img)
     panel.image = img
-    panel.pack()
- 
-root = Tk()     # создаем корневой объект - окно
-root.title("camera tester")     # устанавливаем заголовок окна
-root.geometry("300x250")    # устанавливаем размеры окна
- 
-label = Label(text="здарова бандиты") # создаем текстовую метку
-label.pack()    # размещаем метку в окне
+    panel.pack(anchor=NW)
 
-btn = Button(root, text='open image', command=open_img).pack()
+btn = Button(root, text='Открыть картинку для анализа', command=open_img).pack(anchor=NW)
+
+def analyze():
+    return 0
+
+btn2 = Button(root, text='Анализ', command=analyze)
+
+btn2.place(x=256, y=20)
+
+root.update()
+
+btn2.place(x=256 - btn2.winfo_width(), y=20)
  
 root.mainloop()
